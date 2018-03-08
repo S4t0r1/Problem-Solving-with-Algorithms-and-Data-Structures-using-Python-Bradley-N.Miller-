1428a795f701d2bd93b67a21adb9a25151d41fc1
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-03-08 13:26:12 +0100

    Create LogicGate_experiment.py

---
+++ LogicGate_experiment.py
@@ -0,0 +1,190 @@
+class LogicGate(object):
+    
+    def __init__(self, n):
+        self.name = n
+        self.output = None
+    
+    
+    def getName(self):
+        return self.name
+    
+    
+    def getOutput(self):
+        self.output = self.performGateLogic()
+        return self.output
+
+
+class BinaryGate(LogicGate):
+    
+    def __init__(self, n):
+        LogicGate.__init__(self,n)
+        
+        self.pinA = None
+        self.pinB = None
+    
+    
+    def getPinA(self):
+        if self.pinA is None:
+            return int(input("Enter Pin A (0|1) for " + self.getName() + " --> "))
+        else:
+            return self.pinA.getFrom().getOutput()
+    
+    
+    def getPinB(self):
+        if self.pinB is None:
+            return int(input("Enter Pin B (0|1) for " + self.getName() + " --> "))
+        else:
+            return self.pinB.getFrom().getOutput()
+    
+    
+    def setNextPin(self, source):
+        if self.pinA is None:
+            self.pinA = source
+        else:
+            if self.pinB is None:
+                self.pinB = source
+            else:
+                print("ERROR: All gates full.")
+
+
+class AndGate(BinaryGate):
+    
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+    
+    
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+
+
+class NandGate(AndGate):
+    
+    def performGateLogic(self):
+        if super().performGateLogic() == 1:
+            return 0
+        else:
+            return 1
+
+
+class OrGate(BinaryGate):
+    
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+    
+    
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 or b == 1:
+            return 1
+        else:
+            return 0
+
+
+class NorGate(OrGate):
+    
+    def performGateLogic(self):
+        if super().performGateLogic() == 1:
+            return 0
+        else:
+            return 1
+
+
+# Excercise 10 {..
+class XorGate(OrGate):
+    
+    def performGateLogic(self):
+        if (sum((self.getPinA(), self.getPinB()))) % 2 != 0:
+            return 1
+        else:
+            return 0
+# ..} Excercise 10
+
+
+# Excercise 11 {..
+class Half_adder(BinaryGate):
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+        
+        self.summ = 0
+        self.cout = 0
+    
+    def performGateLogic(self):
+        counter = 0
+        c = 0
+        while counter < 8:
+            a = self.getPinA()
+            b = self.getPinB()
+            self.cout = 1 if (sum((a, b)) == 2) else 0
+            self.summ = 1 if (sum((a, b, c)) % 2 != 0) else 0
+            print("cin = {c} | cout = {self.cout} | sum = {self.summ}"
+                                                  .format(**locals()))
+            c = self.cout
+            counter += 1
+        return self.cout, self.summ
+# ..} Excercise 11
+
+
+class UnaryGate(LogicGate):
+    
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
+        
+        self.pin = None
+    
+    
+    def getPin(self):
+        if self.pin is None:
+            return int(input("Enter Pin (0|1) for " + self.getName() + " --> "))
+        else:
+            return self.pin.getFrom().getOutput()
+    
+    
+    def setNextPin(self, source):
+        if self.pin is None:
+            self.pin = source
+        else:
+            print("ERROR: All gates full.")
+
+
+class NotGate(UnaryGate):
+    
+    def __init__(self, n):
+        UnaryGate.__init__(self, n)
+    
+    
+    def performGateLogic(self):
+        if self.getPin():
+            return 0
+        else:
+            return 1
+
+
+class Connector(object):
+    
+    def __init__(self, fgate, tgate):
+        self.fromgate = fgate
+        self.togate = tgate
+        
+        tgate.setNextPin(self)
+    
+    
+    def getFrom(self):
+        return self.fromgate
+    
+    
+    def getTo(self):
+        return self.togate
+
+
+
+def main():
+    g1 = Half_adder("G1")
+    print(g1.getOutput())
+
+main()
