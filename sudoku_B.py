48e82c55da4ce4baeb132af03cfb6ef3631d9469
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-03-13 17:36:13 +0100

    Update sudoku_B.py

--- sudoku_B.py
+++ sudoku_B.py
@@ -18,7 +18,7 @@ def print_all(all_boards):
     board = build_gameboard()
     all_boards = all_9_boards(board)
     i = 0
-    while i < 9:
+    while i < len(all_boards):
         for n in range(3):
             (r1, r2, r3) = all_boards[i][n], all_boards[i + 1][n], all_boards[i + 2][n]
             (r1, r2, r3) = [" ".join(str(element) for element in string) 
