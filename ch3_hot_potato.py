b4e6f62b791e0807c16e9b1a689e85d5ac28fd21
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-04-05 11:41:51 +0200

    Create ch3_hot_potato.py

---
+++ ch3_hot_potato.py
@@ -0,0 +1,26 @@
+import random
+from pythonds.basic.queue import Queue
+
+
+def num_input():
+    prompt = input("Do you want to submit counter manually?: ")
+    if prompt in {"Y".lower(), "YES".lower()}:
+        return int(input("Choose counter: "))
+    return random.randint(1, 20)
+
+
+def hot_potato(nameslst, num=num_input()):
+    circle = Queue()
+    print("counter is %d" % (num))
+    for name in nameslst:
+        circle.enqueue(name)
+
+    while circle.size() > 1:
+        for i in range(num):
+            circle.enqueue(circle.dequeue())
+        circle.dequeue()
+    return circle.dequeue()
+
+
+nameslst = ["Jan", "Anna", "Peter", "Zuzana", "Martin", "Dominika"]
+print(hot_potato(nameslst))
